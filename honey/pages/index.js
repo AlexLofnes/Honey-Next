22e347539e60ca7e961a7cce5f8d870d32508ffa
Alex Lofnes <alexlofnes@gmail.com>
2022-02-18 15:08:14 +0000

    netlify build

--- honey/pages/index.js
+++ honey/pages/index.js
@@ -4,9 +4,9 @@ export default function IndexPage() {
   return (
     <div className='h-screen bg-[url("../public/mainBg.jpg")] bg-cover bg-top'>
       <Navbar />
-      <div className="flex justify-center h-fit">
-        <div className="flex justify-center w-4/5 h-52 rounded-lg bg-white bg-opacity-30 mt-12">
-        Hello
+      <div className="flex justify-center h-[90%]">
+        <div className="flex justify-center align-middle w-4/5 h-5/6 rounded-2xl bg-white bg-opacity-30 mt-16">
+          <h1 className="text-7xl font-bold">Emery&apos;s Honey</h1>
         </div>
       </div>
       
